973d3441f4bff41f8cced06b07fd95184bd917ea
Mrinal <mrinal.das@connectingpointstech.com>
2025-01-28 17:11:43 +0000

    yes

--- javascript/01_prctc.js
+++ javascript/01_prctc.js
@@ -1,14 +1,30 @@
-const names = ['John', 'Jane', 'Jack', 'Jill', 'Jack'];
+// const names = ['John', 'Jane', 'Jack', 'Jill', 'Jack'];
 
-const new_names = ['Ram', 'Shyam', 'Jitu', 'Janki', 'Dhanu'];
+// const new_names = ['Ram', 'Shyam', 'Jitu', 'Janki', 'Dhanu'];
 
-// names.push(new_names);
-// console.log(names);
+// // names.push(new_names);
+// // console.log(names);
 
-const name1= names.concat(new_names);
-console.log(name1);
+// const name1= names.concat(new_names);
+// console.log(name1);
 
-const name2= [...names,...new_names];
+// const name2= [...names,...new_names];
 
 
-// end
+// // end
+
+function add (n1,n2) {
+    console.log(typeof n1);
+    console.log(typeof n2);
+
+    const r=n1+n2;
+    console.log("Result = ",r);
+    return r;
+}
+// const r2=add(10,20);
+// console.log("Result = ",r2);
+// console.log(add);
+add(5,5)
+
+
+
